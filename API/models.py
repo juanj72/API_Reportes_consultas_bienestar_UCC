9d92928eaed0332bcd106864f73766a93730de0b
juanj72 <juan.jara@campusucc.edu.co>
2023-04-02 16:44:33 -0500

    mostrar la tabla django_migrations definiendo un metamodelo sin migraciones, porque se está usando una base de datos ya existente

--- API/models.py
+++ API/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 
 
+class django_migrations(models.Model):
+    id=models.IntegerField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name=models.CharField(max_length=255)
+    applied =models.DateTimeField()
+    class Meta:
+        db_table='django_migrations'
--- API/views.py
+++ API/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from API.models import django_migrations
+from API.serializers import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
 
 # Create your views here.
+
+
+
+class inicio(generics.ListAPIView):
+    queryset = django_migrations.objects.all()
+    serializer_class=migraciones_serializador
+ 
